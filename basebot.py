CylonicRaider/basebot

commit 9bf846bf1987ce6a49947953573a72fbc6153de2

Actually fulfill promise from documentation

basebot.py
@@ -2294,8 +2294,9 @@ class MiniBot(Bot):
         if not hasattr(self.regexes, 'keys'):
             self.regexes = collections.OrderedDict(self.regexes)
 
-    def handle_chat(self, msg, meta):
-        "See Bot.handle_chat() for details."
+    def handle_chat_ex(self, msg, meta):
+        "See Bot.handle_chat_ex() for details."
+        basebot.Bot.handle_chat_ex(self, msg, meta)
         if (not self.match_self and meta['own'] or
                 meta['edit'] or meta['long']):
             return

commit 386943475db8e4b10a723ed0bde8aa3918cdacdf

Fix bug
Regression introduced in 9bf846bf1987ce6a49947953573a72fbc6153de2.

basebot.py
@@ -2296,7 +2296,7 @@ class MiniBot(Bot):
 
     def handle_chat_ex(self, msg, meta):
         "See Bot.handle_chat_ex() for details."
-        basebot.Bot.handle_chat_ex(self, msg, meta)
+        Bot.handle_chat_ex(self, msg, meta)
         if (not self.match_self and meta['own'] or
                 meta['edit'] or meta['long']):
             return