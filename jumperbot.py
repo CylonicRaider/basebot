b65376ea3cca98494d0d143a06e4a9157aefcb62
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-05-03 13:45:22 +0200

    Update replying code in jumperbot.py

--- jumperbot.py
+++ jumperbot.py
@@ -5,12 +5,12 @@ import basebot
 
 def jump_handler(match, meta):
     room = match.group(1)
-    meta['self'].send_chat('/me jumps away...', meta['msgid'])
+    meta['reply']('/me jumps away...')
     meta['self'].set_roomname(room)
 
 def main():
     basebot.run_minibot(botname='JumperBot', nickname='JumperBot',
-        short_help='I jump into others rooms if commanded to.',
+        short_help='I jump into others rooms when commanded to.',
         long_help='"!jump &roomname" to make me jump there.',
         regexes={'^!jump\s+&?([a-z][a-z0-9]+)\s*$': jump_handler})
 
